d00c6d0d602487ceb049e03bee1cd9140b23cb89
dandy-collaborator-4866 <brobertson@utexas.edu>
2020-12-09 16:10:11 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,9 +5,8 @@ var playlist = {
 playlist.artistName;
 
 function updatePlaylist (playlist, ArtistName, songTitle) {
-  playlist.'Slowdive' = 'Alison'
-  playlist.'My Bloody Valentine' = 'Sometimes'
-  object.assign({}, playlist, {ArtistName: "SongTitle"})
+  playlist.'Slowdive' = 'Alison';
+  playlist.'My Bloody Valentine' = 'Sometimes';
   return playlist;
 }
 
